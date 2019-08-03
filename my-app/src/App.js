9a830ee3cde19124e887fe2b30e19c890a1b6067
puchi2000 <diegovelazqu@hotmail.com>
2019-08-03 12:40:40 -0500

    Llevo desarrollada una pequeña parte de aplicación con react en donde muestra nada más tarjetas con base en un json que elaboré con datos como muestra

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
+import {Component} from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Importando una navegación que hicimos como componente 
+import Navigation from './components/Navigation'; 
+
+
+
+//Vamos a importar los datos prueba
+import {todos} from './toDos.json';
+
+
+//Esto es para crear un componente en React 
+class App extends Component{
+    constructor(){
+	super();
+	this.state = {
+	    todos: todos 
+	}
+    }
+    render(){
+	 
+    //Puedo procesar los datos antes de renderizarlos
+    const todos = this.state.todos.map((todo, i) => {
+	return(
+	    <div className="col-md-4">
+		<div className="card mt-4">
+		   <div className="card-header">
+		       <h3>{todo.title}</h3>
+		       <span className="badge badge-pill badge-danger ml-2">{todo.priority}</span>
+	           </div>
+		   <div className="card-body">
+                       <p>{todo.description}</p>
+		       <p><mark>{todo.responsible}</mark></p>
+	           </div>
+		</div>
+            </div>
+	)
+    })
+     
+    return (
+      <div className="App">
+	    <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white">
+	        Tareas 
+                </a>
+            </nav>
+	    <div className="container">
+	       <div className="row mt-4">
+	           {todos}
+               </div>
+	    </div>
+            <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+    }
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //Se necesita para los trabajos web
+import ReactDOM from 'react-dom'; //Se necesita para los trabajos web 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; //Hace que la aplicación funcione aunque se haya desconectado a internet. 
 
+//Renderiza la aplicación en la pantalla 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
