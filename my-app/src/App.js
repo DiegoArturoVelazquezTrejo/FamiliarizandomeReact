9579934e52e774f747ab07c60c99ae063c86e65f
puchi2000 <diegovelazqu@hotmail.com>
2019-08-03 13:40:31 -0500

    Ya puede agregar tareas a través de la interfaz

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,8 @@ import logo from './logo.svg';
 import './App.css';
 
 //Importando una navegación que hicimos como componente 
-import Navigation from './components/Navigation'; 
-
+//import Navigation from './components/Navigation'; 
+import ToDoForm from './components/ToDoForm';
 
 
 //Vamos a importar los datos prueba
@@ -18,8 +18,17 @@ class App extends Component{
 	super();
 	this.state = {
 	    todos: todos 
-	}
+	};
+	this.handleAddTodo = this.handleAddTodo.bind(this); 
     }
+
+    handleAddTodo(todo){
+	this.setState({
+	    //Agregar una nueva tarea al estado de tareas
+	    todos:[...this.state.todos, todo]
+	})
+    }
+    
     render(){
 	 
     //Puedo procesar los datos antes de renderizarlos
@@ -29,22 +38,30 @@ class App extends Component{
 		<div className="card mt-4">
 		   <div className="card-header">
 		       <h3>{todo.title}</h3>
-		       <span className="badge badge-pill badge-danger ml-2">{todo.priority}</span>
+		       <span className="badge badge-pill badge-danger ml-2">
+		          {todo.priority}
+	               </span>
 	           </div>
+		
 		   <div className="card-body">
                        <p>{todo.description}</p>
-		       <p><mark>{todo.responsible}</mark></p>
-	           </div>
+	       	       <p><mark>{todo.responsible}</mark></p>
+	  	   </div>
+		
 		</div>
             </div>
 	)
-    })
-     
+    }); 
+
+	
     return (
       <div className="App">
 	    <nav className="navbar navbar-dark bg-dark">
                 <a href="" className="text-white">
-	        Tareas 
+	            Tareas
+	            <span className="badge badge-pill badge-light ml-2">
+	                 {this.state.todos.length}
+	           </span>
                 </a>
             </nav>
 	    <div className="container">
@@ -52,7 +69,10 @@ class App extends Component{
 	           {todos}
                </div>
 	    </div>
+	    <div className="col-md-4 text-center">
             <img src={logo} className="App-logo" alt="logo" />
+	    <ToDoForm onAddTodo={this.handleAddTodo} />
+	    </div>
       </div>
     );
     }
